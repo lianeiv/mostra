64d10e091749f525660f507a0c58f30a939aefa9
lianeiv <julianeiv4@gmail.com>
2026-06-23 08:03:51 -0300

    Adiciona a imagem das primeiras suspeitas e inicia o banco de dados

---
+++ database/database.js
@@ -0,0 +1,15 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database/dados.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS votos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      suspeita TEXT NOT NULL,
+      data_voto DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+});
+
+module.exports = db;
